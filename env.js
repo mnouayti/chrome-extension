093104d306424997acc8a63152ccce6537d6cf96
Nouayti Mohammed <med.nouayti@gmail.com>
2018-05-11 02:27:31 +0100

    adding loader and rendering ui depending on login status

---
+++ env.js
@@ -0,0 +1,5 @@
+window.env = {
+    AUTH0_DOMAIN: 'extension.eu.auth0.com',
+    AUTH0_CLIENT_ID: 'oP2bN5A9UyvqgK36Fi8Npulk3A81MY1X',
+  };
+  
--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,10 +1,68 @@
-function isLoggedIn(token){
+function isLoggedIn(token) {
     // The user is logged in if their token isn't expired
     return jwt_decode(token).exp > Date.now() / 1000;
-}
-
-function logout() {
+  }
+  
+  function logout() {
     // Remove the idToken from storage
     localStorage.clear();
     main();
-}
+  }
+  
+  // Minimal jQuery
+  const $$ = document.querySelectorAll.bind(document);
+  const $  = document.querySelector.bind(document);
+  
+  
+  function renderProfileView(authResult) {
+    $('.default').classList.add('hidden');
+    $('.loading').classList.remove('hidden');
+    fetch(`https://${env.AUTH0_DOMAIN}/userinfo`, {
+      headers: {
+        'Authorization': `Bearer ${authResult.access_token}`
+      }
+    }).then(resp => resp.json()).then((profile) => {
+      ['picture', 'name', 'nickname'].forEach((key) => {
+  
+         const element = $('.' +  key);
+         if( element.nodeName === 'DIV' ) {
+           element.style.backgroundImage = 'url(' + profile[key] + ')';
+           return;
+         }
+  
+         element.textContent = profile[key];
+      });
+      $('.loading').classList.add('hidden');
+      $('.profile').classList.remove('hidden');
+      $('.logout-button').addEventListener('click', logout);
+    }).catch(logout);
+  }
+  
+  
+  function renderDefaultView() {
+    $('.default').classList.remove('hidden');
+    $('.profile').classList.add('hidden');
+    $('.loading').classList.add('hidden');
+  
+    $('.login-button').addEventListener('click', () => {
+      $('.default').classList.add('hidden');
+      $('.loading').classList.remove('hidden');
+      chrome.runtime.sendMessage({
+        type: "authenticate"
+      });
+    });
+  }
+  
+  
+  function main () {
+    const authResult = JSON.parse(localStorage.authResult || '{}');
+    const token = authResult.id_token;
+    if (token && isLoggedIn(token)) {
+      renderProfileView(authResult);
+    } else {
+      renderDefaultView();
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', main);
+  
